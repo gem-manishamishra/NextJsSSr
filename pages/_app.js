b91af36a8b2f840795364d52169244401cb65bea
Manisha Mishra <ma.mishra@gemsol.in>
2022-05-18 11:01:24 +0530

    Adding Redux Functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import Layout from '../components/layout'
+import { store,wrapper } from '../redux/store';
+import { Provider } from 'react-redux';
+// import { wrapper} from "../store/store";
+
 function MyApp({ Component, pageProps }) {
-  return ( <><Layout/><Component {...pageProps} /></>)
+  return ( <>
+            <Provider store={store}>
+              <Layout/>
+              <Component {...pageProps} />
+            </Provider> 
+          </>)
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
